59641d82745acbcae8c8a5606dbf10d93a45e1a4
DennyHalim.com <mail2dny@gmail.com>
2026-06-08 16:07:27 +0700

    Update [[route]].js

--- functions/api/[[route]].js
+++ functions/api/[[route]].js
@@ -7,6 +7,7 @@ export async function onRequest(context) {
     'Access-Control-Allow-Methods': 'GET, OPTIONS',
     'Access-Control-Allow-Headers': 'Content-Type'
   };
+  const pretty = (data) => new Response(JSON.stringify(data, null, 2), {headers: cors});
   if (context.request.method === 'OPTIONS') return new Response(null, {headers: cors});
 
   try {
@@ -327,7 +328,7 @@ async function blacklist(ipParam, context, cors) {
       results[list] = {status: 'ERROR', code: null, reason: e.message};
     }
   }
-  return Response.json({ip, results}, {headers: cors});
+  return pretty({ip, results}, {headers: cors});
 }
 
 async function bgp(ipParam, context, cors) {
