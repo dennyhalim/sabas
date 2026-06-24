564f3bebb43a861609cb501596b792a76d544d90
denny <mail2dny@gmail.com>
2026-06-24 16:13:17 +0700

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -565,6 +565,11 @@ function renderHtmlPage(posts, feeds, requestUrl, title) {
     </main>
   </div>
   <img src="https://counter11.optistats.ovh/private/freecounterstat.php?c=tu39n5z56tu56f9judearse1aum3c4qa" border="0">
+  <script>
+    window.goatcounter = {
+        path: function(p) { return location.host + p }
+    }
+</script>
     <script data-goatcounter="https://diselamatkan.goatcounter.com/count"
         async src="//gc.zgo.at/count.js"></script>
 </body>
