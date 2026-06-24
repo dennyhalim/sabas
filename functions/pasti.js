b2d9d820d5fe6981c34b10d23d444c21c96afd7f
denny <mail2dny@gmail.com>
2026-06-24 16:14:12 +0700

    Update pasti.js

--- functions/pasti.js
+++ functions/pasti.js
@@ -443,6 +443,11 @@ async function submitForm() {
 }
 <\/script>
   <img src="https://counter11.optistats.ovh/private/freecounterstat.php?c=tu39n5z56tu56f9judearse1aum3c4qa" border="0">
+  <script>
+    window.goatcounter = {
+        path: function(p) { return location.host + p }
+    }
+</script>
     <script data-goatcounter="https://diselamatkan.goatcounter.com/count"
         async src="//gc.zgo.at/count.js"></script>
 </body>
