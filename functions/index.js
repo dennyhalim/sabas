16da1af6f1a0ac60de3d54346d38ae179d47ea2a
denny <mail2dny@gmail.com>
2026-06-24 14:39:34 +0700

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -564,6 +564,9 @@ function renderHtmlPage(posts, feeds, requestUrl, title) {
       ${postCardsHtml}
     </main>
   </div>
+  <img src="https://counter11.optistats.ovh/private/freecounterstat.php?c=tu39n5z56tu56f9judearse1aum3c4qa" border="0" title="free page counter html code" alt="free page counter html code">
+    <script data-goatcounter="https://diselamatkan.goatcounter.com/count"
+        async src="//gc.zgo.at/count.js"></script>
 </body>
 </html>`;
 }
@@ -824,8 +827,6 @@ Content-Type: text/xml
 &lt;?xml version="1.0"?&gt;
 &lt;opml version="2.0"&gt; … &lt;/opml&gt;</pre>
   </div>
-  <script data-goatcounter="https://diselamatkan.goatcounter.com/count"
-        async src="//gc.zgo.at/count.js"></script>
 </body>
 </html>`, { headers: { 'Content-Type': 'text/html; charset=utf-8' } });
 }
