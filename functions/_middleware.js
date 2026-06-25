8cb3efa13354f04c4d425544305c1d5b8e6a3f92
denny <mail2dny@gmail.com>
2026-06-25 15:04:23 +0700

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,4 +1,15 @@
-export async function onRequest({request}) {
-  const host = request.headers.get('host');
-  return new Response(`Host = ${host}`, {headers:{'Content-Type':'text/plain'}});
+export async function onRequest(context) {
+  const { request } = context;
+  const host = request.headers.get('host').replace('www.', '');
+  
+  // Convert bio.john.com → bio_john_com
+  const safeName = host.replace(/\./g, '_');
+  const funcPath = `/${safeName}.js`;
+  
+  const res = await fetch(new URL(funcPath, new URL(request.url).origin), request);
+  
+  if (res.status === 404) {
+    return fetch(new URL('/default.js', request.url), request);
+  }
+  return res;
 }
