8b5293635ffdb01a0bb320073215dbc24eb9d80c
denny <mail2dny@gmail.com>
2026-06-25 15:03:03 +0700

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,19 +1,4 @@
-export async function onRequest(context) {
-  const { request } = context;
-  const url = new URL(request.url);
-  const host = request.headers.get('host').replace('www.', '');
-
-  // Build path: /bio.john.com.js
-  const funcPath = `/${host}.js`;
-  const targetUrl = new URL(funcPath, url.origin);
-
-  // Try fetch the file. If 404, go to default
-  const res = await fetch(targetUrl, request);
-  
-  if (res.status === 404) {
-    const defaultUrl = new URL('/default.js', url.origin);
-    return fetch(defaultUrl, request);
-  }
-  
-  return res;
+export async function onRequest({request}) {
+  const host = request.headers.get('host');
+  return new Response(`Host = ${host}`, {headers:{'Content-Type':'text/plain'}});
 }
